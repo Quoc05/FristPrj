86cda780c2be9aaa0ac05101997717729a5cfed1
QuocNguyen05 <nguyenvanquoclt1@gmail.com>
2024-08-12 23:03:31 +0700

    Frist Commit

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args){
+      System.out.println("Hello,Toi la Quoc Nguyen!");
+        System.out.println("Welcome to DHKTPM19ATT");
+    }
+
+}
